dd8b78fe494fae0f391d5ea92381b442693c93db
Stevanus Setiawan <157869742+stevanussetiawan@users.noreply.github.com>
2024-05-05 20:05:07 +0700

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from get_isi_berita import GetIsiBerita
+
+df = pd.read_excel("testing_crawling_03_05_2024.xlsx", "data_melanggar")
+isi_links = df["LINK"].unique().tolist()
+list_berita = []
+count = 1
+for link in isi_links:
+    tmp_list = []
+    isi_berita = GetIsiBerita().request_web(link)
+    print(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> {count}/{len(isi_links)}")
+    count += 1
+    tmp_list.append(link)
+    tmp_list.append(isi_berita)
+    list_berita.append(tmp_list)
+    
+df_link_berita = pd.DataFrame(list_berita, columns=["LINK", "ISI BERITA"])
+print(df_link_berita)
+df_link_berita.to_excel("df_links_isi_berita.xlsx")
